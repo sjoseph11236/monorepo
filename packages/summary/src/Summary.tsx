bcbb6c3d47e9016b545ab2b0ba94eb6a6e630331
Sayeed Joseph <sayeedjoseph@gmail.com>
2024-01-13 08:28:12 -0500

    update summary

--- packages/summary/src/Summary.tsx
+++ packages/summary/src/Summary.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 type SummaryProps = {
   title: string;
+  subTitle: string;
 }
 
-export default function Summary({ title }: SummaryProps) {
+export default function Summary({ title, subTitle }: SummaryProps) {
   return (
-    <div>{title}</div>
+    <div>{title}{subTitle}</div>
   )
 }
